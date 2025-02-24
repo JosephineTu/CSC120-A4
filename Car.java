61174d6a0f51e8f3567d68eacb2d1d679bcd9dcb
JosephineTu <jtu@smith.edu>
2025-02-24 12:06:41 -0500

    add comments

--- Car.java
+++ Car.java
@@ -6,19 +6,36 @@ class Car implements CarRequirements{
      */
     private ArrayList<Passenger> passengersOnBoard;
     private int maxCapacity;
-
+    /**
+     * Constructor
+     * @param passengersOnBoard
+     * @param maxCapacity
+     */
     public Car (ArrayList<Passenger> passengersOnBoard, int maxCapacity){
         this.passengersOnBoard=passengersOnBoard;
         this.maxCapacity=maxCapacity;
     }
+    /**
+     * Accessor getCapacity()
+     * @return num, the max capacity
+     */
     public int getCapacity(){
         int num=this.maxCapacity;
         return num;
     }
+    /**
+     * Accessor seatsRemaining()
+     * @return r, the number of seats remaining
+     */
     public int seatsRemaining(){
         int r=this.maxCapacity-this.passengersOnBoard.size();
         return r;
     }
+    /**
+     * Setter addPassenger(Passenger p)
+     * @param Passenger p, the passenger object
+     * @return true or false, indicating whether successfully added or not
+     */
     public Boolean addPassenger(Passenger p){
         if (this.passengersOnBoard.size()<this.maxCapacity){
             this.passengersOnBoard.add(p);
@@ -28,6 +45,10 @@ class Car implements CarRequirements{
             return false;
         }
     }
+    /**
+     * Setter removePassenger(Passenger p)
+     * 
+     */
     public Boolean removePassenger(Passenger p){
         if (this.passengersOnBoard.contains(p)){
             this.passengersOnBoard.remove(p);
@@ -37,37 +58,38 @@ class Car implements CarRequirements{
             return false;
         }
     }
+    /**
+     * Print the names of all the passengers on the car
+     */
     public void printManifest(){
         int i=0;
         for (Passenger p: this.passengersOnBoard){
-            System.out.println(p.name);
+            System.out.println("Passenger"+(i+1)+": "+p.name);
             i++;
         }
     }
+    /**
+     * main 
+     * @param args
+     */
     public static void main(String[] args) {
         Passenger ada = new Passenger("Ada");
         Passenger bob = new Passenger("Bob");
         Passenger carmen = new Passenger("Carmen");
-
-        
+        // create a car object
         ArrayList<Passenger> passengersA = new ArrayList<Passenger>(Arrays.asList(ada, bob, carmen));
         Car a = new Car(passengersA, 5);
-
         // Test getCapacity()
         int capA = a.getCapacity();
         System.out.println("Car a, max capacity: " + capA);
-
         // Test seatsRemaining()
         int seatsA = a.seatsRemaining();
         System.out.println("Car a, seats remaining: " + seatsA);
-
         // Test addPassenger()
         Passenger dan = new Passenger("Dan");
         Boolean m = a.addPassenger(dan);
-
         // Test removePassenger()
         Boolean n = a.removePassenger(bob);  
-
         // Test printManifest()
         a.printManifest();
     }
--- Engine.java
+++ Engine.java
@@ -15,22 +15,39 @@ class Engine implements EngineRequirements{
         this.maxFuelLevel=maxFuelLevel;
     }
     /**
-     * getFuelType
-     * @return FuelType
+     * Getter method getFuelType()
+     * @return FuelType FuelType
      */
     public FuelType getFuelType(){
         return this.f;
     }
+    /**
+     * Getter method getMaxFuel()
+     * @return double maxFuelLevel
+     */
     public double getMaxFuel(){
         return this.maxFuelLevel;
     }
+    /**
+     * Getter getCurrentFuel()
+     * @return double currentFuelLevel
+     */
     public double getCurrentFuel(){
         return this.currentFuelLevel;
     }
+    /**
+     * refuel()
+     * rewrite currentFuelLevel with the value of maxFuelLevel
+     */
     public void refuel(){
         this.currentFuelLevel=this.maxFuelLevel;
     }
-    public Boolean go(){
+    /**
+     * go()
+     * subtract 10.0 from currentFuelLevel
+     * @return Boolean whether need to refuel(false) or not(true)
+     */
+    public Boolean go(){  
         this.currentFuelLevel-=10.0;
         if(this.currentFuelLevel>0.0){
             System.out.println("Fuel remaining:"+this.currentFuelLevel);
@@ -43,10 +60,18 @@ class Engine implements EngineRequirements{
         
     
     }
+    /**
+     * toString() test method converting object attributes into println string
+     */
     public String toString(){
         return ("Engine has fuel type"+this.f+"Max fuel level"+this.maxFuelLevel+"Current fuel level"+this.currentFuelLevel);
     }
+    /**
+     * main method
+     * @param args
+     */
     public static void main(String[] args){
+        //create an Engine object
         Engine myEngine=new Engine(FuelType.ELECTRIC,0.0,1.0);
         String out=myEngine.toString();
         System.out.println(out);
